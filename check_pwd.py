7c179164e35a7d7ebcb291d3da782d19ae46584f
Tianle Yuan <61530469+yuantianle@users.noreply.github.com>
2022-02-13 14:20:32 -0800

    Fix2_bullet2
    Made check_pwd return False if the string do not include lowercase letter.

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,7 @@
+import string
 def check_pwd(num):
     if len(num) < 8 or len(num) > 20:
         return False
-    else:
-        return True
+    if num.isupper():
+        return False
+    return True
