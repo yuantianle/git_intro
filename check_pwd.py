8f57e271276627a5afe7ce89160e08a752bc7a3d
Tianle Yuan <61530469+yuantianle@users.noreply.github.com>
2022-02-13 14:33:55 -0800

    Fix4_bullet4
    Made check_pwd return False if the string do not include digit.

--- check_pwd.py
+++ check_pwd.py
@@ -2,8 +2,18 @@ import string
 def check_pwd(num):
     if len(num) < 8 or len(num) > 20:
         return False
+
     if num.isupper():
         return False
+
     if num.islower():
+        return False
+
+    flag_digit = False
+    for i in num:
+        if (i in string.digits):
+            flag_digit = True
+            break
+    if flag_digit == False:
         return False
     return True
